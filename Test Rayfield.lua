2770469dfeafdd201482361909f508fd6114bc3e
bpadb005 <bpad.b005@gmail.com>
2025-09-15 01:46:27 +0800

    Update Test Rayfield.lua

--- Test Rayfield.lua
+++ Test Rayfield.lua
@@ -1,11 +1,11 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Rayfield Example Window",
+   Name = "Rayfield by Rzq",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "Rayfield Interface Suite",
-   LoadingSubtitle = "by Sirius",
-   ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
+   LoadingSubtitle = "by Rzq",
+   ShowText = "GUI", -- for mobile users to unhide rayfield, change if you'd like
    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
    ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
@@ -39,7 +39,7 @@ local Window = Rayfield:CreateWindow({
 
 local Tab = Window:CreateTab("Tutorial", "activity") -- Title, Image
 
-local Tab = Window:CreateTab("Teleport", "map-pinned") -- Title, Image
+local Tab = Window:CreateTab("Teleport", "map-pin") -- Title, Image
 
 local Tab = Window:CreateTab("Misc", "flame") -- Title, Image
 
