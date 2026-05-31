0185f21bbc408206e543685a1c24ba5b33c63a8b
bpadb005 <bpad.b005@gmail.com>
2026-06-01 00:14:27 +0800

    Update by Rzq.lua

--- Script/Rayfield/by Rzq.lua
+++ Script/Rayfield/by Rzq.lua
@@ -50,22 +50,16 @@ end
 local Button = Tab:CreateButton({
    Name = "Spawn",
    Callback = function()
-      teleportPosition(Vector3.new(717.30, 101.00, -890.05))
+      teleportPosition(Vector3.new(335.15, 15.72, 181.39))
    end,
 })
 
 local Section = Tab:CreateSection("Auto")
 
 local teleportList = {
-   Vector3.new(717.30, 101.00, -890.05),
-   Vector3.new(84.41, 141.00, 463.26),
-   Vector3.new(-401.28, 273.00, 772.38),
-   Vector3.new(-588.52, 385.00, -118.45),
-   Vector3.new(-487.00, 281.36, -356.03),
-   Vector3.new(-1291.93, 353.00, -538.26),
-   Vector3.new(-1798.55, 597.00, -436.68),
-   Vector3.new(-3347.49, 685.00, -728.86),
-   Vector3.new(-3366.14, 695.54, -755.33),
+   Vector3.new(329.54, 15.52, 172.52),
+   Vector3.new(324.65, 11.59, 144.86),
+   Vector3.new(320.81, 11.61, 133.51),
 }
 
 local function teleportPositions(pos)
@@ -81,9 +75,33 @@ local respawnAfterLast = false
 local teleportDelay = 3
 
 local Toggle = Tab:CreateToggle({
-   Name = "Auto Teleport",
+   Name = "Auto Teleport 1x",
    CurrentValue = false,
    Flag = "Toggle1",
+   Callback = function(Value)
+        if Value then
+         task.spawn(function()
+            for _, pos in ipairs(teleportList) do
+               teleportPositions(pos)
+
+               local loopCount = math.floor(teleportDelay * 10)
+               for i = 1, loopCount do
+                  task.wait(0.1)
+               end
+            end
+
+            pcall(function()
+               Rayfield.Flags.Toggle1:Set(false)
+            end)
+         end)
+      end
+   end,
+})
+
+local Toggle = Tab:CreateToggle({
+   Name = "Auto Teleport",
+   CurrentValue = false,
+   Flag = "Toggle2",
    Callback = function(Value)
         autoTeleport = Value
 
@@ -123,7 +141,7 @@ local Toggle = Tab:CreateToggle({
 local Toggle = Tab:CreateToggle({
    Name = "Auto Respawn",
    CurrentValue = false,
-   Flag = "Toggle2",
+   Flag = "Toggle3",
    Callback = function(Value)
         respawnAfterLast = Value
    end,
@@ -142,6 +160,125 @@ local Slider = Tab:CreateSlider({
 })
 
 local Tab = Window:CreateTab("Misc", "flame")
+local Section = Tab:CreateSection("Player")
+
+local currentWalkSpeed = 16
+local currentJumpPower = 50
+
+local WalkSpeedSlider = Tab:CreateSlider({
+   Name = "Walk Speed (default 16)",
+   Range = {0, 300},
+   Increment = 1,
+   Suffix = " studs/detik",
+   CurrentValue = 16,
+   Flag = "Slider2",
+   Callback = function(Value)
+        currentWalkSpeed = Value
+
+        local Character = game.Players.LocalPlayer.Character
+        local Humanoid = Character and Character:FindFirstChildOfClass("Humanoid")
+
+        if Humanoid then
+            Humanoid.WalkSpeed = Value
+        end
+   end,
+})
+
+local JumpPowerSlider = Tab:CreateSlider({
+   Name = "Jump Power (default 50)",
+   Range = {0, 500},
+   Increment = 1,
+   Suffix = " power",
+   CurrentValue = 50,
+   Flag = "Slider3",
+   Callback = function(Value)
+        currentJumpPower = Value
+
+        local Character = game.Players.LocalPlayer.Character
+        if Character and Character:FindFirstChild("Humanoid") then
+            Character.Humanoid.UseJumpPower = true
+            Character.Humanoid.JumpPower = Value
+        end
+   end,
+})
+
+local player = game.Players.LocalPlayer
+
+player.CharacterAdded:Connect(function(character)
+    local humanoid = character:WaitForChild("Humanoid")
+
+    humanoid.UseJumpPower = true
+    humanoid.WalkSpeed = currentWalkSpeed
+    humanoid.JumpPower = currentJumpPower
+end)
+
+local Button = Tab:CreateButton({
+   Name = "Reset To Default",
+   Callback = function()
+      currentWalkSpeed = 16
+      currentJumpPower = 50
+
+      WalkSpeedSlider:Set(16)
+      JumpPowerSlider:Set(50)
+
+      local Character = game.Players.LocalPlayer.Character
+      local Humanoid = Character and Character:FindFirstChildOfClass("Humanoid")
+
+      if Humanoid then
+         Humanoid.WalkSpeed = 16
+         Humanoid.UseJumpPower = true
+         Humanoid.JumpPower = 50
+      end
+   end,
+})
+
+game.Players.LocalPlayer.CharacterAdded:Connect(function(Character)
+   local Humanoid = Character:WaitForChild("Humanoid")
+
+   Humanoid.UseJumpPower = true
+   Humanoid.WalkSpeed = currentWalkSpeed
+   Humanoid.JumpPower = currentJumpPower
+end)
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local InfiniteJumpEnabled = false
+local InfiniteJumpConnection
+
+local Toggle = Tab:CreateToggle({
+   Name = "Infinite Jump",
+   CurrentValue = false,
+   Flag = "Toggle4",
+   Callback = function(Value)
+      InfiniteJumpEnabled = Value
+
+      if Value then
+         if not InfiniteJumpConnection then
+            InfiniteJumpConnection = game:GetService("UserInputService").JumpRequest:Connect(function()
+               if InfiniteJumpEnabled then
+                  local Character = LocalPlayer.Character
+                  local Humanoid = Character and Character:FindFirstChild("Humanoid")
+
+                  if Humanoid then
+                     Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                  end
+               end
+            end)
+         end
+      end
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Disable Gameplay Paused",
+   Callback = function()
+      game
+         :GetService("CoreGui")
+         .RobloxGui["CoreScripts/NetworkPause"]
+         :Destroy()
+   end,
+})
 
 local Tab = Window:CreateTab("Information", "info")
 local Section = Tab:CreateSection("About Me")
